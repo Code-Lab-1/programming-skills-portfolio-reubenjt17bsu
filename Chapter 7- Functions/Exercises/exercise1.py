8a165fc9e59f26f1818bd4559820f50b066d57e6
reubenjt17bsu <reubentitus17@gmail.com>
2022-11-25 22:01:03 +0400

    Solution

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,7 @@
+#Exercise No:1
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to love the creative computing coding."
+    print(msg)
+
+display_message()
